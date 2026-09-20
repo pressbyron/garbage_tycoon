62490fe9a6992eac61ad7ef45db7cb35577e83c1
pressbyron <peter.jaaskelainen@hotmail.com>
2026-09-20 23:14:42 +0200

    Big update
    Bug fixes
    Trees around map
    Recycling center asset update
    Trash landfill upgrade

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789332899|4663082';
+const CACHE_VERSION = '1789938821|5445246';
 /** @type {string} */
 const CACHE_PREFIX = 'GarbageTruckTyco-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
