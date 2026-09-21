78d9c74422776ea372e9fa2920b135566050e9a2
pressbyron <peter.jaaskelainen@hotmail.com>
2026-09-21 13:39:22 +0200

    Update alot of things

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789938821|5445246';
+const CACHE_VERSION = '1789990696|4934024';
 /** @type {string} */
 const CACHE_PREFIX = 'GarbageTruckTyco-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
